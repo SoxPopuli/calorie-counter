d202952502588c30ad07b90896cad5ad139199b3
Charlotte Smith <charliesmith5019@gmail.com>
2023-06-29 23:27:40 +0100

    setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,8 +11,5 @@ fn hello(name: &str) -> Result<String, String> {
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![hello])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    app::AppBuilder::new().run();
 }
